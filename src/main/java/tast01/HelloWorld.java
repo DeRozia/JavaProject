043fa88faecb908d38f47a0778277082eb6b9a45
beautylulu <2021302111401@whu.edu.cn>
2022-09-13 19:32:31 +0800

    wrote a h flie

---
+++ src/main/java/tast01/HelloWorld.java
@@ -0,0 +1,7 @@
+package JavaProject;
+
+public class HelloWorld {
+    public static void main(String[] args){
+      System.out.println("Hello,I am the best student!");
+    }
+}
---
+++ src/main/java/tast01/MyInformation.java
@@ -0,0 +1,2 @@
+package tast01;public class MyInformation {
+}
