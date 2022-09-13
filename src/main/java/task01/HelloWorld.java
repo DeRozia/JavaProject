c4ef88a09129ed7868a014d3e931d4a41c5822aa
beautylulu <2021302111401@whu.edu.cn>
2022-09-13 20:45:34 +0800

    update

---
+++ src/main/java/task01/HelloWorld.java
@@ -0,0 +1,9 @@
+package task01;
+
+public class HelloWorld{
+   public static void main(String[] args){
+     //静态main函数
+     System.out.println("Hello,I am the best student!");//打印Hello
+   }
+}
+
---
+++ src/main/java/task01/MyInformation.java
@@ -0,0 +1,10 @@
+package task01;
+public class MyInformation {
+    public static void main(String[] args){
+        //静态main函数
+        System.out.println("我的姓名是：刘子谦");//打印姓名
+        System.out.println("我的性别是：男");//打印性别
+        System.out.println("我的籍贯是：湖北");//打印籍贯
+        System.out.println("我的住址是：武汉大学信息学部");//打印住址
+    }
+}
